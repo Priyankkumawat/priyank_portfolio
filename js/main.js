260e4ec8c7e87ce6d0197ed569055c3bb6018e02
Galactus__3000 <priyankkumawat7223@gmail.com>
2022-07-19 23:34:27 +0530

    v1

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let nav = document.querySelector(".navbar");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }
+    else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// nav hide 
+let navbar = document.querySelectorAll(".nav-link")
+let navcollapse = document.querySelector(".navbar-collapse.collapse");
+navbar.forEach(function(a){
+    a.addEventListener("click",function(){
+        navcollapse.classList.remove("show");
+    })
+}
+)
